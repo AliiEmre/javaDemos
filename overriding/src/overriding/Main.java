3b2ad21746ef8e635d0f8adfe4c5ffc7a98ce622
emrey <emrey@192.168.1.27>
2022-10-10 21:54:15 +0300

    Homework 3

---
+++ overriding/src/overriding/Main.java
@@ -0,0 +1,14 @@
+package overriding;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BaseKrediManager[] krediManagers = new BaseKrediManager[] {new OgretmenKrediManager(), new TarimKrediManager(), new OgrenciKrediManager()};
+		
+		for (BaseKrediManager KrediManager : krediManagers) {
+			System.out.println(KrediManager.hesapla(1000));
+		}
+
+	}
+
+}
